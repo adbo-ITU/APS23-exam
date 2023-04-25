f7dd7b758a8f3722218712906423ab332147a8b9
Adrian Borup <adrian@borup.com>
2023-04-25 16:21:10 +0200

    Add untested input format validator

---
+++ kattis-problem-design/input_format_validators/validate.py
@@ -0,0 +1,35 @@
+#! /usr/env/python3
+
+import sys
+import re
+import argparse
+
+
+parser = argparse.ArgumentParser('Input format validator')
+parser.add_argument('--min-r', type=int, default=1)
+parser.add_argument('--max-r', type=int, default=100)
+parser.add_argument('--min-c', type=int, default=1)
+parser.add_argument('--max-c', type=int, default=100)
+parser.add_argument('--min-b', type=int, default=1)
+parser.add_argument('--max-b', type=int, default=100)
+args = parser.parse_args()
+
+
+line = sys.stdin.readline()
+
+assert re.match(r"[1-9][0-9]* [1-9][0-9]* [1-9][0-9]*\n", line), line
+
+r, c, b = map(int, line.split())
+
+assert args.min_r <= r <= args.max_r
+assert args.min_c <= c <= args.max_c
+assert args.min_b <= b <= args.max_b
+
+for i in range(r):
+    line = sys.stdin.readline()
+    assert re.match(r"^([#\.]+)\n", line), i
+    assert len(line.strip()) == c, i
+
+assert sys.stdin.readline() == ""
+
+sys.exit(42)
